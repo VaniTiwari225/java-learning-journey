4b86e399394aba219a0a02f65af628dbea704973
vani tiwari <tiwarivani70@gmail.com>
2026-06-30 15:15:44 +0530

    function code

--- javapractice/src/functions/Printsum.java
+++ javapractice/src/functions/Printsum.java
@@ -35,3 +35,17 @@ class Printsub{
         return sub;
     }
 }
+
+//add two numbers using fucntions and methods
+class Swap{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        add(a,b);
+    }
+    static void add(int a,int b){
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
