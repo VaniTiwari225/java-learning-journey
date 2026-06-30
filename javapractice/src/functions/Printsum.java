16d84f3a0801140cc84deb1371d27bec60015e7d
vani tiwari <tiwarivani70@gmail.com>
2026-06-30 15:01:07 +0530

    function code

--- javapractice/src/functions/Printsum.java
+++ javapractice/src/functions/Printsum.java
@@ -1,3 +1,4 @@
+//print the sum of two numbers in such way that you can repeat the method as many times you want
 package functions;
 import java.util.*;
 public class Printsum{
@@ -15,4 +16,22 @@ public class Printsum{
         System.out.println("sum :"+sum);
     }
 
+}
+
+
+//print subtract of two numbers
+class Printsub{
+    public static void main(String[] args) {
+        int substraction=sub();
+        System.out.println(substraction);
+    }
+    static int sub(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter first no. :");
+        int a=sc.nextInt();
+        System.out.println("enter the second no. :");
+        int b=sc.nextInt();
+        int sub=a-b;
+        return sub;
+    }
 }
