fc5f28841245c4130408d12da20fe0cca054d3e4
vani tiwari <tiwarivani70@gmail.com>
2026-06-30 14:47:31 +0530

    function code

--- javapractice/src/functions/Printsum.java
+++ javapractice/src/functions/Printsum.java
@@ -1,13 +1,18 @@
 package functions;
 import java.util.*;
-public class Printsum {
+public class Printsum{
     public static void main(String[] args) {
+        sum();
+        sum();
+    }
+    static void sum(){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter first number :");
+        System.out.println("enter first no. :");
         int a=sc.nextInt();
-        System.out.println("enter the second number :");
+        System.out.println("enter second no. :");
         int b=sc.nextInt();
         int sum=a+b;
         System.out.println("sum :"+sum);
     }
+
 }
