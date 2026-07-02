953c9c699291d2a4fa60830c217d9b6778246307
vani tiwari <tiwarivani70@gmail.com>
2026-07-02 11:59:31 +0530

    array code

---
+++ javapractice/src/twodarrays/numbers.java
@@ -0,0 +1,22 @@
+package twodarrays;
+import java.util.*;
+public class numbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+         int numbers[][]=new int[r][c];
+         for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                numbers[i][j]=sc.nextInt();
+            }
+         }
+         //output
+         for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                System.out.print(numbers[i][j]+" ");
+            }
+            System.out.println();
+         }
+    }
+}
