301191a715b900dad8e74ccb42ede03f7a4e2a9b
vani tiwari <tiwarivani70@gmail.com>
2026-07-02 09:47:09 +0530

    array code

---
+++ javapractice/src/arrays/Marks.java
@@ -0,0 +1,13 @@
+package arrays;
+import java.util.*;
+public class Marks {
+    public static void main(String[] args) {
+        int[] marks=new int[3];
+        marks[0]=45;
+        marks[1]=76;
+        marks[2]=34;
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
