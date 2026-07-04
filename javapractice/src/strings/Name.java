415813e9e088cdf9f93c1881ff8a873378b6ae99
vani tiwari <tiwarivani70@gmail.com>
2026-07-04 11:09:28 +0530

    string code

---
+++ javapractice/src/strings/Name.java
@@ -0,0 +1,9 @@
+package strings;
+import java.util.*;
+public class Name {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        System.out.println("my name is :"+str);
+    }
+}
