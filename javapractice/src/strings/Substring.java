686a7deca885885ad61b359fb3d46ce6ab17e6f9
vani tiwari <tiwarivani70@gmail.com>
2026-07-04 12:06:54 +0530

    string code

---
+++ javapractice/src/strings/Substring.java
@@ -0,0 +1,10 @@
+package strings;
+import java.util.*;
+public class Substring {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String str2=str.substring(7,10);
+        System.out.println(str2);
+    }
+}
