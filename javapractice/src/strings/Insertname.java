bbae8dadccb3f9630f7b3e5bc504ba210b042599
vani tiwari <tiwarivani70@gmail.com>
2026-07-07 16:06:21 +0530

    string code

---
+++ javapractice/src/strings/Insertname.java
@@ -0,0 +1,13 @@
+//Insert your name at the beginning of a sentence.
+package strings;
+import java.util.*;
+public class Insertname {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String sentence=sc.nextLine();
+        String name=sc.next();
+        StringBuilder sb=new StringBuilder(sentence);
+        sb.insert(2,name+" ");
+        System.out.println(sb);
+    }
+}
